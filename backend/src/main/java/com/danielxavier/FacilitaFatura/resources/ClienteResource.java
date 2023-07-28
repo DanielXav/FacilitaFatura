8a3a56ae4768c2d1a6b06a81283907ee394ec779
Daniel Xavier <dxavier194@gmail.com>
2023-07-28 16:34:12 -0300

    Update cliente

--- backend/src/main/java/com/danielxavier/FacilitaFatura/resources/ClienteResource.java
+++ backend/src/main/java/com/danielxavier/FacilitaFatura/resources/ClienteResource.java
@@ -4,11 +4,10 @@ import com.danielxavier.FacilitaFatura.dto.ClienteDTO;
 import com.danielxavier.FacilitaFatura.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +27,20 @@ public class ClienteResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClienteDTO> findById(@PathVariable Long id){
         ClienteDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> insert(@RequestBody ClienteDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClienteDTO> insert(@PathVariable Long id, @RequestBody ClienteDTO dto){
+        dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
 }
--- backend/src/main/java/com/danielxavier/FacilitaFatura/services/ClienteService.java
+++ backend/src/main/java/com/danielxavier/FacilitaFatura/services/ClienteService.java
@@ -2,8 +2,9 @@ package com.danielxavier.FacilitaFatura.services;
 
 import com.danielxavier.FacilitaFatura.dto.ClienteDTO;
 import com.danielxavier.FacilitaFatura.entities.Cliente;
-import com.danielxavier.FacilitaFatura.exceptions.EntityNotFoundException;
+import com.danielxavier.FacilitaFatura.exceptions.ResourceNotFoundException;
 import com.danielxavier.FacilitaFatura.repositories.ClienteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,5 +29,26 @@ public class ClienteService {
         Optional<Cliente> obj = repository.findById(id);
         Cliente entity = obj.orElseThrow(() -> new EntityNotFoundException("Cliente não encontrado!"));
         return new ClienteDTO(entity);
+    }
+
+    @Transactional
+    public ClienteDTO insert(ClienteDTO dto){
+        Cliente entity = new Cliente();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new ClienteDTO(entity);
+    }
+
+    @Transactional
+    public ClienteDTO update(Long id, ClienteDTO dto) {
+        try {
+            Cliente entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new ClienteDTO(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id não encontrado " + id);
+        }
     }
 }
