6ae5cbc1481e24fcbc19497b2ca9614fb9b260ed
Daniel Xavier <dxavier194@gmail.com>
2023-09-11 12:08:28 -0300

    Pagination

--- backend/src/main/java/com/danielxavier/FacilitaFatura/resources/ClienteResource.java
+++ backend/src/main/java/com/danielxavier/FacilitaFatura/resources/ClienteResource.java
@@ -3,6 +3,9 @@ package com.danielxavier.FacilitaFatura.resources;
 import com.danielxavier.FacilitaFatura.dto.ClienteDTO;
 import com.danielxavier.FacilitaFatura.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -19,8 +22,16 @@ public class ClienteResource {
     private ClienteService service;
 
     @GetMapping
-    public ResponseEntity<List<ClienteDTO>> findAll(){
-        List<ClienteDTO> list = service.findAll();
+    public ResponseEntity<Page<ClienteDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        Page<ClienteDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
--- backend/src/main/java/com/danielxavier/FacilitaFatura/services/ClienteService.java
+++ backend/src/main/java/com/danielxavier/FacilitaFatura/services/ClienteService.java
@@ -8,6 +8,8 @@ import com.danielxavier.FacilitaFatura.repositories.ClienteRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,9 +24,9 @@ public class ClienteService {
     private ClienteRepository repository;
 
     @Transactional(readOnly = true)
-    public List<ClienteDTO> findAll(){
-        List<Cliente> list = repository.findAll();
-        return list.stream().map(ClienteDTO::new).toList();
+    public Page<ClienteDTO> findAllPaged(PageRequest pageRequest){
+        Page<Cliente> list = repository.findAll(pageRequest);
+        return list.map(ClienteDTO::new);
     }
 
     @Transactional(readOnly = true)
@@ -67,4 +69,5 @@ public class ClienteService {
             throw new DatabaseException("Falha na integridade referencial");
         }
     }
+
 }
